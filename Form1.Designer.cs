97d72cb291215553994a4506dc0b36fecdd341ed
Md Junain Muntasir <junainmuntasir@gmail.com>
2024-09-13 16:02:55 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ratPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ratPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ratPictureBox
+            // 
+            this.ratPictureBox.Image = global::WindowsFormsApp1.Properties.Resources.rat_image;
+            this.ratPictureBox.Location = new System.Drawing.Point(251, 145);
+            this.ratPictureBox.Name = "ratPictureBox";
+            this.ratPictureBox.Size = new System.Drawing.Size(100, 58);
+            this.ratPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ratPictureBox.TabIndex = 0;
+            this.ratPictureBox.TabStop = false;
+            this.ratPictureBox.Click += new System.EventHandler(this.ratPictureBox_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(251, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 1500;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ratPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ratPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ratPictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch watch = new Stopwatch();
+        Random random = new Random();
+        int[,] captureData = new int[10, 3];
+        int captureCount = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameTimer.Interval = 1500;
+            gameTimer.Start();
+            watch.Start();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            int maxX = this.Size.Width - ratPictureBox.Width;
+            int maxY = this.Size.Height - ratPictureBox.Height;
+
+            int randomX = random.Next(0, maxX);
+            int randomY = random.Next(0, maxY);
+
+            ratPictureBox.Location = new Point(randomX, randomY);
+        }
+
+        private void ratPictureBox_Click(object sender, EventArgs e)
+        {
+            int totalX = 0, totalY = 0;
+            double totalIndex = 0.0;
+
+            int x = ratPictureBox.Location.X;
+            int y = ratPictureBox.Location.Y;
+            double elapsedTime = watch.ElapsedMilliseconds;
+            double timeInterval = gameTimer.Interval;
+            MessageBox.Show($"Congratulations! You caught the Rat. Time elapse is: {elapsedTime}");// Time since the last capture
+
+            if (captureCount < 10)
+            {
+                captureData[captureCount, 0] = x; 
+                captureData[captureCount, 1] = y;
+                captureData[captureCount, 2] = (int)Math.Log(elapsedTime) /(int) Math.Log(gameTimer.Interval);
+                captureCount++;
+            }
+            if (captureCount == 10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    totalX += captureData[i, 0];
+                    totalY += captureData[i, 1];
+                    totalIndex += captureData[i, 2];
+                }
+
+                int avgX = totalX / 10;
+                int avgY = totalY / 10;
+                double avgIndex = totalIndex / 10;
+
+                MessageBox.Show($"Average X: {avgX}, Average Y: {avgY}, Average Index: {avgIndex}");
+                gameTimer.Stop();
+            }
+            
+            watch.Restart();
+            if (gameTimer.Interval>250)
+            {
+                gameTimer.Interval -= 250;
+            }
+        }
+    }
+    
+}
